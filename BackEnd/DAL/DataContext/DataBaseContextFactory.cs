3ce7a0431398b2aca0f4f95acea7bacd6a44c51b
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-10-20 12:06:15 -0300

    Configurando la Clase : DataBaseContextFactory

--- BackEnd/DAL/DataContext/DataBaseContextFactory.cs
+++ BackEnd/DAL/DataContext/DataBaseContextFactory.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//agregamos estos using
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace DAL.DataContext
 {
-    public class DataBaseContextFactory
+    //aplicamos la interfaz : IDesignTimeDbContextFactory<DataBaseContext>
+    public class DataBaseContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
     {
-        
+        public DataBaseContext CreateDbContext(string[] args)
+        {
+            AppConfiguration appConfiguration = new AppConfiguration();
+            var opsBuilder = new DbContextOptionsBuilder<DataBaseContext>();
+            opsBuilder.UseSqlServer(appConfiguration.SqlConnectionString);
+
+            return new DataBaseContext(opsBuilder.Options);
+        }
     }
 }
