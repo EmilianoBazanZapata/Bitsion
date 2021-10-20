9c8da44f020d5585b671c132c09abee42c41aaae
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-10-20 11:41:59 -0300

    Configurando la clase : AppConfiguration

--- BackEnd/DAL/DataContext/AppConfiguration.cs
+++ BackEnd/DAL/DataContext/AppConfiguration.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//agregamos estos using
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.DataContext
 {
     public class AppConfiguration
     {
-        
+        //constructor
+        public AppConfiguration()
+        {
+            var ConfigBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            ConfigBuilder.AddJsonFile(path, false);
+            var root = ConfigBuilder.Build();
+            //ubicacion de la cadena de conexion hacia la BD
+            var AppSettings = root.GetSection("ConnectionStrings:BD");
+            SqlConnectionString = AppSettings.Value;
+        }
+        public string SqlConnectionString { get; set; }
     }
 }
